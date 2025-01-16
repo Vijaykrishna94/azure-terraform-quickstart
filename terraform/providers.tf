fd7a5514c6010db6eee859fe7cf64fe6cb7d7b0c
Vijay Burra <vijay.burra@datatalks.se>
2025-01-16 14:45:19 +0100

    main and provider

---
+++ terraform/providers.tf
@@ -0,0 +1,25 @@
+# tells tf which providers are necessary
+# azure rm from HashiCorp Provider Registry - 1.e.- Code
+# Random for pet names
+# ~> any version of 3
+terraform {
+  required_providers {
+    azurerm ={
+        source = "hashicorp/azurerm"
+        version = "~>3.0"
+    }
+    random ={
+    source = "hashicorp/random"
+    version="~>3.0"
+    } 
+  }
+}
+
+# We want to connect to Azure
+# 
+provider "azurerm" {
+    features {
+      
+    }
+  
+}
